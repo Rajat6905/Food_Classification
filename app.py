af20abcb6ade2be9263acf6686305e73607c9327
rajat <srajat490@gmail.com>
2019-06-17 11:10:45 +0530

    .

---
+++ app.py
@@ -0,0 +1,109 @@
+import os
+import tensorflow as tf
+graph = tf.get_default_graph()
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug import secure_filename
+from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
+from keras.models import Sequential, load_model
+from keras.preprocessing import image as image_utils
+import numpy as np
+import argparse
+import time
+import uuid
+import base64
+import numpy as np
+
+img_width, img_height = 128, 128
+model_path = './models/model.h5'
+model_weights_path = './models/weights.h5'
+model = load_model(model_path)
+model.load_weights(model_weights_path)
+model._make_predict_function()
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg'])
+
+def get_as_base64(url):
+    return base64.b64encode(requests.get(url).content)
+
+def predict(file):
+	global graph
+	with graph.as_default():
+		x = load_img(file, target_size=(128,128))
+		x = img_to_array(x)
+		x = np.expand_dims(x, axis=0)
+		array = model.predict(x)
+		result = array[0]
+		answer = np.argmax(result)
+		if answer == 0:
+		    print('french fries')
+		elif answer ==1:
+		    print('pizza')
+		elif answer == 2:
+		    print('samosa')
+		return answer
+
+
+def my_random_string(string_length=10):
+    """Returns a random string of length string_length."""
+    random = str(uuid.uuid4()) # Convert UUID format to a Python string.
+    random = random.upper() # Make all characters uppercase.
+    random = random.replace("-","") # Remove the UUID '-'.
+    return random[0:string_length] # Return the random string.
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route("/")
+def template_test():
+    return render_template('template.html', label='', imagesource='../uploads/template.jpg')
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        import times
+        start_time = time.time()
+        file = request.files['file']
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            print(file_path)
+            result = predict(file_path)
+            print(result)
+            if result == 0:
+                label = 'french fries'
+            elif result == 1:
+                label = 'pizza'			
+            elif result == 2:
+                label = 'samosa'
+            print(result)
+            print(file_path)
+            filename = my_random_string(6) + filename
+
+            os.rename(file_path, os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            print("--- %s seconds ---" % str (time.time() - start_time))
+            return render_template('template.html', label=label, imagesource='../uploads/' + filename)
+
+from flask import send_from_directory
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+from werkzeug import SharedDataMiddleware
+app.add_url_rule('/uploads/<filename>', 'uploaded_file',
+                 build_only=True)
+app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+    '/uploads':  app.config['UPLOAD_FOLDER']
+})
+
+if __name__ == "__main__":
+    
+    app.run()
